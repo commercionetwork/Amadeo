16169f2583d233a7fed9213b5433c81c6837154a
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-15 14:57:25 +0200

    Use RecipientAddressTextFieldWidget, add default fields

--- lib/pages/1-commercio-account/send_tokens_page.dart
+++ lib/pages/1-commercio-account/send_tokens_page.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 import 'package:amadeo_flutter/pages/section_page.dart';
 import 'package:amadeo_flutter/widgets/base_scaffold_widget.dart';
 import 'package:amadeo_flutter/widgets/paragraph_widget.dart';
+import 'package:amadeo_flutter/widgets/recipient_address_text_field_widget.dart';
 import 'package:commercio_ui/commercio_ui.dart';
 import 'package:commercio_ui/ui/account/commercio_account_ui.dart';
+import 'package:commerciosdk/export.dart' as sdk;
 import 'package:flutter/material.dart';
 
 class SendTokensPage extends SectionPageWidget {
@@ -28,7 +30,7 @@ class SendTokensPageBody extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: Center(
             child: Column(
-              children: const [
+              children: [
                 SendTokensWidget(),
               ],
             ),
@@ -40,15 +42,42 @@ class SendTokensPageBody extends StatelessWidget {
 }
 
 class SendTokensWidget extends StatelessWidget {
-  const SendTokensWidget();
+  SendTokensWidget({
+    this.defaultAmount = '100',
+    this.defaultDenom = 'ucommercio',
+  })  : amountTextController = TextEditingController(text: defaultAmount),
+        denomTextController = TextEditingController(text: defaultDenom);
+
+  final String defaultAmount;
+  final String defaultDenom;
+  final TextEditingController recipientTextController = TextEditingController();
+  final TextEditingController amountTextController;
+  final TextEditingController denomTextController;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
         children: [
+          RecipientAddressTextFieldWidget(
+            recipientTextController: recipientTextController,
+          ),
+          TextField(
+            controller: amountTextController,
+            decoration: InputDecoration(
+              hintText: defaultAmount,
+              labelText: 'Amount of tokens',
+            ),
+          ),
+          TextField(
+            controller: denomTextController,
+            decoration: InputDecoration(
+              hintText: defaultDenom,
+              labelText: 'Amount denom',
+            ),
+          ),
           const ParagraphWidget(
-            'Press the button to send 100 ucommercio coins.',
+            'Press the button to send the selected amount of coins.',
             padding: EdgeInsets.all(5.0),
           ),
           SendTokensFlatButton(
@@ -58,8 +87,12 @@ class SendTokensWidget extends StatelessWidget {
               'Sending...',
               style: TextStyle(color: Colors.white),
             ),
-            recipientAddress: 'did:com:14ttg3eyu88jda8udvxpwjl2pwxemh72w0grsau',
-            amount: const [CommercioCoin(amount: '100')],
+            recipientAddress: recipientTextController.text.split(',')[0].trim(),
+            amount: [
+              sdk.StdCoin(
+                  denom: denomTextController.text,
+                  amount: amountTextController.text)
+            ],
             color: Theme.of(context).primaryColor,
             disabledColor: Theme.of(context).primaryColorDark,
           ),
