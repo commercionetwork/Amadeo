666fa728d95cfc0bf5e8bfb7785dad37ae4e88b1
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-17 15:29:35 +0200

    Add net switcher (see #3)

--- lib/widgets/base_app_bar_widget.dart
+++ lib/widgets/base_app_bar_widget.dart
@@ -1,12 +1,19 @@
+import 'package:amadeo_flutter/helpers/net_helper.dart';
+import 'package:commercio_ui/commercio_ui.dart';
+import 'package:commercio_ui/core/utils/export.dart';
+import 'package:commerciosdk/export.dart' as sdk;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BaseAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   final String title;
-  final AppBar appBar;
   final List<Widget> widgets;
 
-  const BaseAppBarWidget({Key key, this.title, this.appBar, this.widgets})
-      : super(key: key);
+  const BaseAppBarWidget({
+    Key key,
+    this.title,
+    this.widgets,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +27,50 @@ class BaseAppBarWidget extends StatelessWidget implements PreferredSizeWidget {
           color: Colors.white,
         ),
       ),
-      actions: widgets,
+      actions: widgets ??
+          [
+            Padding(
+              padding: const EdgeInsets.only(right: 12.0),
+              child: PopupMenuButton<ChainNet>(
+                onSelected: (ChainNet result) {
+                  final commercioAccount =
+                      RepositoryProvider.of<StatefulCommercioAccount>(context);
+
+                  commercioAccount.networkInfo = sdk.NetworkInfo(
+                    bech32Hrp: result.bech32Hrp,
+                    lcdUrl: result.lcdUrl,
+                  );
+
+                  commercioAccount.httpHelper = HttpHelper(
+                    faucetDomain: result.faucetDomain,
+                    lcdUrl: result.lcdUrl,
+                  );
+
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Switched to ${result.name}'),
+                      backgroundColor: Colors.green,
+                    ),
+                  );
+                },
+                itemBuilder: (_) => [
+                  PopupMenuItem<ChainNet>(
+                    value: ChainNet.test,
+                    child: Text(ChainNet.test.name),
+                  ),
+                  PopupMenuItem<ChainNet>(
+                    value: ChainNet.dev,
+                    child: Text(ChainNet.dev.name),
+                  ),
+                ],
+                initialValue: ChainNet.dev,
+                child: const Icon(Icons.settings),
+              ),
+            ),
+          ],
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(appBar.preferredSize.height);
+  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
 }
--- lib/widgets/base_scaffold_widget.dart
+++ lib/widgets/base_scaffold_widget.dart
@@ -1,28 +1,23 @@
+import 'package:amadeo_flutter/widgets/base_app_bar_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'package:amadeo_flutter/widgets/base_app_bar_widget.dart';
 
 class BaseScaffoldWidget extends StatelessWidget {
   final List<Widget> appBarWidgets;
   final Widget bodyWidget;
 
-  const BaseScaffoldWidget(
-      {Key key, this.appBarWidgets, @required this.bodyWidget})
-      : super(key: key);
+  const BaseScaffoldWidget({
+    Key key,
+    @required this.bodyWidget,
+    this.appBarWidgets,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BaseAppBarWidget(
-        title: 'The Amadeo App',
-        appBar: AppBar(),
-        widgets: appBarWidgets,
-      ),
+      appBar: BaseAppBarWidget(title: 'Amadeo', widgets: appBarWidgets),
       body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return bodyWidget;
-        },
+        builder: (_, __) => bodyWidget,
       ),
     );
   }
