d293372f95fcdd1854cdedf16125e8e33fd4c934
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-19 16:34:47 +0200

    Add docId and documentContent fields

--- lib/repositories/document_repository.dart
+++ lib/repositories/document_repository.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/services.dart';
+import 'package:uuid/uuid.dart';
 
 class DocumentRepository {
   final AssetBundle assetBundle;
   final String documentPath;
+  String documentContent;
+  String docId;
 
   DocumentRepository({
     AssetBundle assetBundle,
     String documentPath,
   })  : assetBundle = assetBundle ?? rootBundle,
-        documentPath = documentPath ?? './assets/document.txt';
+        documentPath = documentPath ?? './assets/document.txt',
+        docId = '';
+
+  bool get hasLoadedDocument => documentContent != null;
+
+  bool get hasNotLoadedDocument => !hasLoadedDocument;
+
+  bool get hasDocIdGenerated => docId.isEmpty;
+
+  bool get hasNotDocIdGenerated => !hasDocIdGenerated;
 
   Future<ByteData> fetchDocument() => assetBundle.load(documentPath);
 
-  Future<String> fetchContent() => assetBundle.loadString(documentPath);
+  Future<String> fetchContent() async =>
+      documentContent = await assetBundle.loadString(documentPath);
+
+  String generateNewDocId() => docId = Uuid().v4();
 }
