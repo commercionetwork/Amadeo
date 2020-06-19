ce5e9024b5d9198af1299a71413a741eff1c9122
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-19 16:39:06 +0200

    Remove prints in bloc delegate

--- lib/simple_bloc_delegate.dart
+++ lib/simple_bloc_delegate.dart
@@ -1,21 +1,3 @@
 import 'package:bloc/bloc.dart';
 
-class SimpleBlocDelegate extends BlocDelegate {
-  @override
-  void onEvent(Bloc bloc, Object event) {
-    print('onEvent $event');
-    super.onEvent(bloc, event);
-  }
-
-  @override
-  onTransition(Bloc bloc, Transition transition) {
-    print('onTransition $transition');
-    super.onTransition(bloc, transition);
-  }
-
-  @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    print('onError $error');
-    super.onError(bloc, error, stackTrace);
-  }
-}
+class SimpleBlocDelegate extends BlocDelegate {}
