c0050cd8de6eb6515776ef9ab8963aa71f1e182b
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-06-25 16:43:42 +0200

    Fix spacing

--- lib/widgets/paragraph_widget.dart
+++ lib/widgets/paragraph_widget.dart
@@ -4,11 +4,13 @@ class ParagraphWidget extends StatelessWidget {
   final String paragraph;
   final EdgeInsetsGeometry padding;
 
-  const ParagraphWidget(this.paragraph,
-      {this.padding = const EdgeInsets.symmetric(
-        vertical: 8.0,
-        horizontal: 16.0,
-      )});
+  const ParagraphWidget(
+    this.paragraph, {
+    this.padding = const EdgeInsets.symmetric(
+      vertical: 8.0,
+      horizontal: 16.0,
+    ),
+  });
 
   @override
   Widget build(BuildContext context) {
