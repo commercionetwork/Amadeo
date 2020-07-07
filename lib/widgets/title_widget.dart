7405553d85a6ef147958fb093282a077f87c31cc
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-07-07 14:41:01 +0200

    Add optional padding

--- lib/widgets/title_widget.dart
+++ lib/widgets/title_widget.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 class TitleWidget extends StatelessWidget {
   final String title;
+  final EdgeInsetsGeometry padding;
 
-  const TitleWidget(this.title);
+  const TitleWidget(this.title, {this.padding});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
+      padding: padding ?? const EdgeInsets.all(8.0),
       child: Text(
         title,
         style: const TextStyle(
