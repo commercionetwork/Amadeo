b1835a29c7d961a17e7e9c24d42350f9f7f706db
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-07-09 09:02:05 +0200

    Rename pages/membership to pages/kyc

--- lib/pages/export.dart
+++ lib/pages/export.dart
@@ -3,5 +3,5 @@ export '2-commercio-id/export.dart';
 export '3-commercio-docs/export.dart';
 export '4-commercio-sign/export.dart';
 export '5-commercio-mint/export.dart';
-export '6-commercio-membership/export.dart';
+export '6-commercio-kyc/export.dart';
 export 'section_page.dart';
