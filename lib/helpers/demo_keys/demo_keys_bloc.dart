0330d4d6a0b6459dd23beffab3002ac684161cc1
Enrico Pasquali <pasquali.enrico94@gmail.com>
2020-07-09 13:34:27 +0200

    Add demo keys bloc & repo

---
+++ lib/helpers/demo_keys/demo_keys_bloc.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:amadeo/repositories/keys_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:commercio_ui/core/core.dart';
+import 'package:commercio_ui/entities/commercio_id_keys.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'demo_keys_event.dart';
+part 'demo_keys_state.dart';
+
+class DemoKeysBloc extends Bloc<DemoKeysEvent, DemoKeysState> {
+  final KeysRepository keysRepository;
+  final StatefulCommercioId commercioIdKeys;
+
+  DemoKeysBloc({
+    @required this.keysRepository,
+    @required this.commercioIdKeys,
+  }) : super(const DemoKeysInitial());
+
+  @override
+  Stream<DemoKeysState> mapEventToState(
+    DemoKeysEvent event,
+  ) async* {
+    if (event is LoadDemoKeysEvent) {
+      yield* _mapLoadDemoKeysEventToState(event);
+    }
+  }
+
+  Stream<DemoKeysState> _mapLoadDemoKeysEventToState(
+    LoadDemoKeysEvent event,
+  ) async* {
+    try {
+      yield const DemoKeysLoading();
+
+      final demoKeys = await keysRepository.fetchDemoKeys();
+      commercioIdKeys.commercioIdKeys = demoKeys;
+
+      yield DemoKeysData(commercioIdKeys: demoKeys);
+    } catch (e) {
+      yield DemoKeysError(e.toString());
+    }
+  }
+}
